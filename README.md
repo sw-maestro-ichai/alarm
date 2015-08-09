e65aed06a1426e9db437d8818c40133b88fbc3ef
monibu1548 <monibu1548@gmail.com>
2015-08-09 18:25:17 +0900

    Add mysql query function in dht11 module

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# alarm
-Alarm
+# 뭉게뭉게
+## 개요
+
+## 개발환경
+
+## 메뉴얼
+### Raspberry pi
+### MobileApp
+### 구름 제작
--- Rpi-Server/DHT11Sensor/dht.py
+++ Rpi-Server/DHT11Sensor/dht.py
@@ -2,9 +2,15 @@
 import sys
 import time
 import Adafruit_DHT
+import MySQLdb
 
 sensor = Adafruit_DHT.DHT11
 pin = 4
+
+db = MySQLdb.connect("localhost", "root", "thak12!@", "Setting")
+cursor = db.cursor()
+
+
 
 
 temperature, humidity = Adafruit_DHT.read_retry(sensor, pin) 
@@ -12,6 +18,15 @@ temperature, humidity = Adafruit_DHT.read_retry(sensor, pin)
 if temperature is not None and humidity is not None:
 	print 'Humidity = {0:0.1f}% '.format(temperature)
 	print 'Temperature = {0:0.1f}*C ' .format(humidity)
+	cursor.excute("UPDATE Setting SET temperature = {0:0.1f}, humidity = {0:0.1f};".format(temperature, humidity))
+	db.commit()
+
+
+
 
 else: print 'Cant get sensor data!'
+
+
+
+db.close()
 
